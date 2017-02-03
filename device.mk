9ae031fc13f06f6eb2f217ab5fce95d850ce4698
andr68rus <ashtmb@gmail.com>
2017-02-03 20:57:59 +0300

    Add config selective-spn-conf.xml

--- device.mk
+++ device.mk
@@ -124,6 +124,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
 
+# SPN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
